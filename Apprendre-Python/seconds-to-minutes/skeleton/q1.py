4addb53198985ad3e93677469cc7b2f2df541ac1
Sébastien Combéfis <seb478@gmail.com>
2015-10-05 21:34:00 +0200

    Add "Seconds to minutes" Python task

---
+++ Apprendre-Python/seconds-to-minutes/skeleton/q1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Minutes to seconds
+
+def simplify(time):
+@    @f1@@
+    if 'minutes' not in locals() or 'seconds' not in locals():
+        raise pythia.UndeclaredException('undeclared')
+    elif type(minutes) != int or type(seconds) != int:
+        raise pythia.BadTypeException('badtype')
+    return (minutes, seconds)
