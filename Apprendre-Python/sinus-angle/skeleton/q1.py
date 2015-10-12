bf60be7edc31a15c4eacbb20c23dbfa6bff0ab3f
Sébastien Combéfis <seb478@gmail.com>
2015-10-12 17:17:20 +0200

    Update "sinus angle" Python task

--- Apprendre-Python/sinus-angle/skeleton/q1.py
+++ Apprendre-Python/sinus-angle/skeleton/q1.py
@@ -2,12 +2,17 @@
 # Course: Apprendre Python
 # Problem: Sinus angle
 
+import sys
+
+sys.path.append('/task/static')
+from lib import pythia
+
 @@imports@@
 
 def sumsinus(a, b):
 @    @f1@@
     if 'result' not in locals():
-        raise pythia.UndeclaredException('undeclared')
-    elif type(result) != float:
-        raise pythia.BadTypeException('badtype')
+        raise pythia.UndeclaredException('result')
+    if type(result) != float:
+        raise pythia.BadTypeException('result', type(result), float)
     return result
