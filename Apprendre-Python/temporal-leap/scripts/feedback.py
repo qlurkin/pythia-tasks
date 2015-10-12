a551c41b659e421c1b3208a8681c1a1cb065c5d3
Sébastien Combéfis <seb478@gmail.com>
2015-10-12 10:41:59 +0200

    Add "temporal leap" python task

---
+++ Apprendre-Python/temporal-leap/scripts/feedback.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Temporal leap
+# Feedback script
+
+import ast
+import csv
+import json
+import os
+import sys
+
+sys.path.append('/task/static')
+from lib import pythia
+
+import math
+
+def computeend(hour, minute, second, duration):
+  endminute = minute + (duration % 60)
+  endhour = hour + (duration // 60) + (endminute // 60)
+  return (endhour, endminute % 60)
+
+class TaskFeedbackSuite(pythia.FeedbackSuite):
+  def __init__(self, config):
+    pythia.FeedbackSuite.__init__(self, '/tmp/work/input/data.csv', '/tmp/work/output/data.res', config)
+
+  def teacherCode(self, data):
+    return computeend(*data)
+
+  def parseTestData(self, data):
+    return tuple(int(x) for x in data)
+
+# Retrieve task id
+with open('/tmp/work/tid', 'r', encoding='utf-8') as file:
+    tid = file.read()
+output = {'tid': tid, 'status': 'failed', 'feedback': {'score': 0}}
+
+# Check stderr
+hasError = False
+stderr = '/tmp/work/output/stderr'
+if os.path.exists(stderr):
+    with open(stderr, 'r') as file:
+        content = file.read().strip()
+        if content != '':
+            output['feedback'] = {'score': 0, 'message': '<p>Une erreur s\'est produite lors de l\'exécution de votre programme :</p><pre>' + content + '</pre>'}
+            hasError = True
+
+if not hasError:
+  # Read test configuration
+  config = []
+  with open('/task/config/test.json', 'r', encoding='utf-8') as file:
+    content = file.read()
+    config = json.loads(content)
+    config = config['predefined']
+  (verdict, feedback) = TaskFeedbackSuite(config).generate()
+  output['feedback'] = feedback
+  output['feedback']['score'] = feedback['stats']['succeeded'] / feedback['stats']['total']
+  output['status'] = 'success' if verdict else 'failed'
+
+print(json.dumps(output))
---
+++ Apprendre-Python/temporal-leap/skeleton/q1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Temporal leap
+
+def computeend(hour, minute, second, duration):
+@    @f1@@
+    if 'endhour' not in locals() or 'endminute' not in locals():
+        raise pythia.UndeclaredException('undeclared')
+    elif type(endhour) != int or type(endminute) != int:
+        raise pythia.BadTypeException('badtype')
+    return (endhour, endminute)
