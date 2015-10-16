8138a315fdaf86570af1e9c78a74c8938e3390b2
Sébastien Combéfis <seb478@gmail.com>
2015-10-16 10:21:04 +0200

    Add "Nombre de lettres" Python task

---
+++ Apprendre-Python/nombre-lettres/scripts/feedback.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Nombre de lettres
+# Feedback script
+
+import ast
+import csv
+import json
+import os
+import sys
+
+sys.path.append('/task/static')
+from lib import pythia
+
+import math
+
+def nbletters(word):
+    return len(set(word))
+
+class TaskFeedbackSuite(pythia.FeedbackSuite):
+    def __init__(self, config):
+        pythia.FeedbackSuite.__init__(self, '/tmp/work/output/stderr', None, '/tmp/work/input/data.csv', '/tmp/work/output/data.res', config)
+
+    def teacherCode(self, data):
+        return nbletters(data)
+
+    def parseTestData(self, data):
+        return data[0]
+
+# Retrieve task id
+with open('/tmp/work/tid', 'r', encoding='utf-8') as file:
+    tid = file.read()
+output = {'tid': tid, 'status': 'failed', 'feedback': {'score': 0}}
+
+# Read test configuration
+config = []
+with open('/task/config/test.json', 'r', encoding='utf-8') as file:
+    content = file.read()
+    config = json.loads(content)
+    config = config['predefined']
+(verdict, feedback) = TaskFeedbackSuite(config).generate()
+output['feedback'] = feedback
+output['status'] = 'success' if verdict else 'failed'
+
+print(json.dumps(output))
---
+++ Apprendre-Python/nombre-lettres/skeleton/q1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Nombre de lettres
+
+import sys
+
+sys.path.append('/task/static')
+from lib import pythia
+
+def nbletters(word):
+    s = '''@@f1@@'''
+    if 'while' in s or 'for' in s:
+        raise Exception('Vous ne pouvez pas utiliser de boucle while ou for')
+    del(s)
+@    @f1@@
