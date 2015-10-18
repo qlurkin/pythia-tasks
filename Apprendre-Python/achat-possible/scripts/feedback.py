c494d32f56f318299ae47fb481e46af0c2eb8665
Sébastien Combéfis <seb478@gmail.com>
2015-10-18 19:39:31 +0200

    Add "Achat possible" Python task

---
+++ Apprendre-Python/achat-possible/scripts/feedback.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Achat possible
+# Feedback script
+
+import ast
+import csv
+import json
+import os
+import sys
+
+sys.path.append('/task/static')
+from lib import pythia
+
+import math
+
+def canbuy(unitprice, quantity, money):
+    return unitprice * quantity <= money
+
+class TaskFeedbackSuite(pythia.FeedbackSuite):
+    def __init__(self, config):
+        pythia.FeedbackSuite.__init__(self, '/tmp/work/output/stderr', None, '/tmp/work/input/data.csv', '/tmp/work/output/data.res', config)
+
+    def teacherCode(self, data):
+        return canbuy(*data)
+
+    def parseTestData(self, data):
+        return tuple(int(x) for x in data)
+
+# Retrieve task id
+with open('/tmp/work/tid', 'r', encoding='utf-8') as file:
+    tid = file.read()
+output = {'tid': tid, 'status': 'failed', 'feedback': {'score': 0}}
+
+# Read test configuration
+config = []
+with open('/task/config/test.json', 'r', encoding='utf-8') as file:
+    content = file.read()
+    config = json.loads(content)
+    config = config['predefined']
+(verdict, feedback) = TaskFeedbackSuite(config).generate()
+output['feedback'] = feedback
+output['status'] = 'success' if verdict else 'failed'
+
+print(json.dumps(output))
---
+++ Apprendre-Python/achat-possible/skeleton/q1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Achat possible
+
+import sys
+
+sys.path.append('/task/static')
+from lib import pythia
+
+def canbuy(unitprice, quantity, money):
+@    @f1@@
+    if 'buy' not in locals():
+        raise pythia.UndeclaredException('buy')
+    if type(buy) != bool:
+        raise pythia.BadTypeException('buy', type(buy), bool)
+    return buy
