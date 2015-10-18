639e888b39d4f099d2da434535d7486e0fa14b96
Sébastien Combéfis <seb478@gmail.com>
2015-10-18 19:40:18 +0200

    Add "Trop petit ou trop grand" Python task

---
+++ Apprendre-Python/trop-petit-ou-grand/skeleton/q1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Trop petit ou trop grand
+
+import sys
+
+sys.path.append('/task/static')
+from lib import pythia
+
+def isforbidden(height):
+@    @f1@@
+    if 'forbidden' not in locals():
+        raise pythia.UndeclaredException('forbidden')
+    if type(forbidden) != bool:
+        raise pythia.BadTypeException('forbidden', type(forbidden), bool)
+    return forbidden
