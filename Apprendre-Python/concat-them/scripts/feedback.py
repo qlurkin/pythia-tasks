390c426aaac2a5b65ec588e358aefdd44bca430d
Sébastien Combéfis <seb478@gmail.com>
2015-10-31 22:07:56 +0100

    Add "Concaténez-les !" Python task

---
+++ Apprendre-Python/concat-them/scripts/feedback.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Concaténez-les !
+# Feedback script
+
+import ast
+import csv
+import json
+import os
+import sys
+
+sys.path.append('/task/static')
+from lib import pythia
+
+import math
+
+def concat(hour, minute, second):
+    return str(hour) + 'h' + str(minute) + ' (' + str(second) + 's)'
+
+class TaskFeedbackSuite(pythia.FeedbackSuite):
+    def __init__(self, config):
+        pythia.FeedbackSuite.__init__(self, '/tmp/work/output/stderr', None, '/tmp/work/input/data.csv', '/tmp/work/output/data.res', config)
+
+    def teacherCode(self, data):
+        return concat(*data)
+
+    def parseTestData(self, data):
+        return tuple(int(x) for x in data)
+
+# Retrieve task id
+with open('/tmp/work/tid', 'r', encoding='utf-8') as file:
+    tid = file.read()
+output = {'tid': tid, 'status': 'failed', 'feedback': {'score': 0}}
+
+# Read test configuration
+config = []
+with open('/task/config/test.json', 'r', encoding='utf-8') as file:
+    content = file.read()
+    config = json.loads(content)
+    config = config['predefined']
+(verdict, feedback) = TaskFeedbackSuite(config).generate()
+output['feedback'] = feedback
+output['status'] = 'success' if verdict else 'failed'
+
+print(json.dumps(output))
---
+++ Apprendre-Python/concat-them/skeleton/q1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Concaténez-les !
+
+import sys
+
+sys.path.append('/task/static')
+from lib import pythia
+
+def concat(hour, minute, second):
+@    @f1@@
+    if 'result' not in locals():
+        raise pythia.UndeclaredException('result')
+    if type(result) != str:
+        raise pythia.BadTypeException('result', type(result), str)
+    return result
