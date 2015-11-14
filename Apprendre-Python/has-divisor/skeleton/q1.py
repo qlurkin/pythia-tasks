5dd3f546749aaf34008905377b9768bd809e7c19
Sébastien Combéfis <seb478@gmail.com>
2015-11-14 18:27:05 +0100

    Add "Le premier diviseur" Python task

---
+++ Apprendre-Python/has-divisor/skeleton/q1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Le premier diviseur
+
+import sys
+
+sys.path.append('/task/static')
+from lib import pythia
+
+def hasdivisor(n, d):
+    divisor = True
+    i = 0
+    while i < n:
+        i += 1
+@    @f1@@
+    return (i, divisor)
