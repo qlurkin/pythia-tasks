b44a99f101199df3c9ee2e9d6cec84595770e85b
Sébastien Combéfis <seb478@gmail.com>
2015-12-29 09:52:10 +0100

    Improve "Coefficient binomial" Python task
    - Added quality check about the number of times fact function has been called with different inputs

--- Apprendre-Python/binomial-coefficient/scripts/feedback.py
+++ Apprendre-Python/binomial-coefficient/scripts/feedback.py
@@ -13,7 +13,6 @@ import sys
 sys.path.append('/task/static')
 from lib import pythia
 
-import math
 import utils
 
 def binom(n, k):
@@ -50,5 +49,27 @@ with open('/task/config/test.json', 'r', encoding='utf-8') as file:
 (verdict, feedback) = TaskFeedbackSuite(config).generate()
 output['feedback'] = feedback
 output['status'] = 'success' if verdict else 'failed'
+
+# Update score with quality constraint
+with open('/tmp/work/output/postprocess.res', 'r', encoding='utf-8') as resfile,\
+     open('/tmp/work/output/postprocess.fdk', 'r', encoding='utf-8') as fdkfile:
+    results = [int(x.strip()) for x in resfile.readlines()]
+    feedbacks = [int(x.strip()) for x in fdkfile.readlines()]
+    if len(results) == len(feedbacks):
+        nocall = 0
+        morecalls = 0
+        i = 0
+        while i < len(results):
+            if results[i] == 0:
+                nocall += 1
+            elif results[i] > feedbacks[i]:
+                morecalls += 1
+            i += 1
+        if nocall > 0:
+            output['feedback']['quality'] = {'weight': (len(results) - nocall) / len(results), 'message': '<p>Vous n\'appelez pas toujours la fonction <code>fact</code>.</p>'}
+        elif morecalls > 0:
+            output['feedback']['quality'] = {'weight': (len(results) - morecalls) / len(results), 'message': '<p>Vous calculez la factorielle de certains nombres inutilement.</p>'}
+    else:
+        output['feedback']['quality'] = {'weight': 0, 'message': '<p>Erreur lors du comptage des appels à la fonction <code>fact</code>.</p>'}
 
 print(json.dumps(output))
--- Apprendre-Python/binomial-coefficient/scripts/utils.py
+++ Apprendre-Python/binomial-coefficient/scripts/utils.py
@@ -1,6 +1,7 @@
 def init():
     global __data__
     __data__ = {}
+    __data__[0] = 1
 
 def fact(n):
     if n not in __data__:
@@ -8,9 +9,10 @@ def fact(n):
     return __data__[n]
 
 def __factrec(n):
-    if n == 0:
-        return 1
-    return n * __factrec(n - 1)
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    return fact
 
 def cache():
     return __data__
