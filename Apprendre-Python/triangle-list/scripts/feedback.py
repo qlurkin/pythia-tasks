79ea3f9aa42c4dea2bededa0d427db94dd080667
Quentin Lurkin <qlurkin@gmail.com>
2016-12-21 18:04:15 +0100

    Add triangle list problem

---
+++ Apprendre-Python/triangle-list/scripts/feedback.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Triangle List
+# Feedback script
+
+import json
+import sys
+
+sys.path.append('/task/static')
+from lib import pythia
+
+def triangle(n):
+    return [list(range(1,i+1)) for i in range(1, n+1)]
+
+class TaskFeedbackSuite(pythia.FeedbackSuite):
+    def __init__(self, config):
+        pythia.FeedbackSuite.__init__(self, '/tmp/work/output/stderr', None, '/tmp/work/input/data.csv', '/tmp/work/output/data.res', config)
+
+    def teacherCode(self, data):
+        return triangle(data)
+
+    def parseTestData(self, data):
+        return data[0]
+
+# Retrieve task id
+with open('/tmp/work/tid', 'r', encoding='utf-8') as file:
+    tid = file.read()
+output = {'tid': tid, 'status': 'failed', 'feedback': {'score': 0}}
+
+# Read test configuration
+config = []
+with open('/task/config/test.json', 'r', encoding='utf-8') as file:
+    content = file.read()
+    config = json.loads(content)
+    config = config['predefined']
+(verdict, feedback) = TaskFeedbackSuite(config).generate()
+output['feedback'] = feedback
+output['status'] = 'success' if verdict else 'failed'
+
+print(json.dumps(output))
---
+++ Apprendre-Python/triangle-list/skeleton/q1.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Triangle List
+
+@@f1@@
