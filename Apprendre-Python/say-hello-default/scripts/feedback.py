22b067f861328fb12f5c9322c3126d10a2b60698
Quentin Lurkin <qlurkin@gmail.com>
2016-12-21 13:54:22 +0100

    Add say hello default problem

---
+++ Apprendre-Python/say-hello-default/scripts/feedback.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Course: Apprendre Python
+# Problem: Say Hello Default
+# Feedback script
+
+import json
+import sys
+
+sys.path.append('/task/static')
+from lib import pythia
+
+def sayhello(name="World"):
+    return 'Hello ' + name + '!'
+
+class TaskFeedbackSuite(pythia.FeedbackSuite):
+    def __init__(self, config):
+        pythia.FeedbackSuite.__init__(self, '/tmp/work/output/stderr', None, '/tmp/work/input/data.csv', '/tmp/work/output/data.res', config)
+
+    def teacherCode(self, data):
+        return sayhello(data)
+
+    def parseTestData(self, data):
+        return data[0]
+
+# Retrieve task id
+with open('/tmp/work/tid', 'r', encoding='utf-8') as file:
+    tid = file.read()
+output = {'tid': tid, 'status': 'failed', 'feedback': {'score': 0}}
+
+# Read test configuration
+config = []
+with open('/task/config/test.json', 'r', encoding='utf-8') as file:
+    content = file.read()
+    config = json.loads(content)
+    config = config['predefined']
+(verdict, feedback) = TaskFeedbackSuite(config).generate()
+output['feedback'] = feedback
+output['status'] = 'success' if verdict else 'failed'
+
+print(json.dumps(output))
